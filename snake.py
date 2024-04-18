ed5718af2452ae25b6e1e8f71fbf330ba6d998f0
Victor Candido <victorjrcandido@gmail.com>
2024-04-18 16:04:29 -0300

    1.3

--- snake.py
+++ snake.py
@@ -3,8 +3,14 @@ import curses
 def game_loop(window):
     window.addstr(f'Aperte uma tecla')
     while True:
+        window.timeout(1000)
         char = window.getch()
-        window.addstr(f'Tecla Pressionada: {char}')
+        window.clear()
+        if char != -1:
+            window.addstr(f'Tecla Pressionada: {char}')
+        else:
+            window.addstr(f'Nenhuma tecla pressionada')
+
 
 if __name__ == '__main__':
     curses.wrapper(game_loop)
